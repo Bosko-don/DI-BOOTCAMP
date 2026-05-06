0228de8b2f3d7ba817bfbaf1e83d5aa6d1bb25a5
BOOTCAMP <donb69968@gmail.com>
2026-05-06 09:57:07 +0300

    Delete week 7 node & react directory

--- week 7 node & react/day 2/exercise 20xp/exercise 2/blog-api/server/Models/booksModel.js
+++
@@ -1,5 +0,0 @@
-let books = [
-  { id: 1, title: "Book One", author: "John", publishedYear: 2020 },
-];
-
-module.exports = { books };
--- week 7 node & react/day 2/exercise 20xp/exercise 2/blog-api/server/routes/booksRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ctrl = require("../controllers/booksController");
-
-router.get("/", ctrl.getBooks);
-router.get("/:bookId", ctrl.getBook);
-router.post("/", ctrl.createBook);
-
-module.exports = router;
